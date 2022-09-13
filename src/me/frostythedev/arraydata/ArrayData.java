95b849b7c19cc3587838b92b73fd35af2e7f4d9e
J. Thompson <jalisa.thompson1@mycavehill.uwi.edu>
2022-09-13 18:55:49 -0400

    Jalisa's Work

--- src/me/frostythedev/arraydata/ArrayData.java
+++ src/me/frostythedev/arraydata/ArrayData.java
@@ -4,7 +4,7 @@ public class ArrayData {
 
     public static void main(String[] args){
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, my Name is...");
     }
 
 }
