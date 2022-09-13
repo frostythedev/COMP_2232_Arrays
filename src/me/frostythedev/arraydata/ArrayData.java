e80abdf33e555dabccd251fc05ee1c7b875e078f
Tevin <frostythedev@gmail.com>
2022-09-13 18:41:31 -0400

    Base Fields

--- src/me/frostythedev/arraydata/ArrayData.java
+++ src/me/frostythedev/arraydata/ArrayData.java
@@ -2,9 +2,16 @@ package me.frostythedev.arraydata;
 
 public class ArrayData {
 
+    int rows;
+    int columns;
+    int values[][];
+    int rowData[];
+    int colData[];
+
     public static void main(String[] args){
 
         System.out.println("Hello World");
+
     }
 
 }
