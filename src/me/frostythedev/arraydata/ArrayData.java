e12daafd69e53c1cf0613c89e72a8889a22d00af
Tevin <frostythedev@gmail.com>
2022-09-29 16:11:07 -0400

    Final setup up base classes and testing

--- src/me/frostythedev/arraydata/ArrayData.java
+++ src/me/frostythedev/arraydata/ArrayData.java
@@ -1,17 +1,35 @@
 package me.frostythedev.arraydata;
 
+import java.lang.reflect.Array;
+
 public class ArrayData {
 
-    int rows;
-    int columns;
-    int values[][];
-    int rowData[];
-    int colData[];
+    private static int rows = 10;
+    private static int columns = 10;
+    
+    private static int values[][] = new int[rows][columns];
+    
+    private static int rowData[] = new int[rows];
+    
+    private static int colData[] = new int[columns];
 
     public static void main(String[] args){
 
-        System.out.println("Hello World");
+        Tevin tevin = new Tevin(rows, columns, values, rowData, colData);
+        Jalisa jalisa = new Jalisa(rows, columns, values, rowData, colData);
+        Simone simone = new Simone(rows, columns, values, rowData, colData);
 
+        System.out.println("TEVIN'S TESTING OUTPUT:");
+        tevin.test();
+        System.out.println("========== \n");
+
+        System.out.println("JALISA'S TESTING OUTPUT:");
+        jalisa.test();
+        System.out.println("========== \n");
+
+        System.out.println("SIMONE'S TESTING OUTPUT:");
+        simone.test();
+        System.out.println("========== \n");
     }
 
 }
